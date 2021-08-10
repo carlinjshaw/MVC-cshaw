3ce8bd638daaa4c8f854c20dfc3c413be4e0bffd
carlinjshaw <carlinjshaw@gmail.com>
2021-08-09 21:09:14 -0400

    config, connection, .env, installed everything, and homepage route

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const path = require("path");
+const fs = require("fs");
+
+// import sequelize connection
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+//uses express handlebars as template engine
+
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+//session and sequelize stuff
+
+const session = require("express-session");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+
+app.use(routes);
+//join the public styles and logic
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
+});
