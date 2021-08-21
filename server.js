df4614d98e306b894188fdff6ea5cee4905eb440
carlinjshaw <carlinjshaw@gmail.com>
2021-08-21 13:09:37 -0400

    reorganized server.js

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const routes = require("./controllers");
-const sequelize = require("./config/connection");
 const path = require("path");
-const fs = require("fs");
-
-// import sequelize connection
+const session = require("express-session");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// import sequelize connection
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
 
 //uses express handlebars as template engine
 
@@ -24,21 +38,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 //session and sequelize stuff
 
-const session = require("express-session");
 
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
-const sess = {
-  secret: "Super secret secret",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
-
-app.use(session(sess));
 
 
 app.use(routes);
